6898d9ffa3967bcbd51f3e6f9c23d64637d1c301
fukuyama247 <fukuyama247@gmai.com>
2024-10-18 16:34:12 +0900

    スクリプト調整、アニメーターファイル作成

--- Assets/CharacterControlles/CharaMove.cs
+++ Assets/CharacterControlles/CharaMove.cs
@@ -8,8 +8,8 @@ public class CharaMove : MonoBehaviour
     private Vector3 PlayerVelocity;
     private bool groundedPlayer;
     public float playerSpeed = 5.0f;
-    public float jumpHeight = 0.85f;
-    private float gravity = -9.81f;
+    public float jumpHeight = 0.1f;
+    public float gravity = -121f;
     public float rotSpeed = 700f;
     // Start is called before the first frame update
     void Start()
@@ -26,20 +26,17 @@ public class CharaMove : MonoBehaviour
             PlayerVelocity.y = 0f;
         }
 
-        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"),0,0);
         charaCon.Move(dir*Time.deltaTime*playerSpeed);
 
-        if(dir != Vector3.zero)
+       
+        if(Input.GetKey(KeyCode.Space)&&groundedPlayer)
         {
-            Quaternion qua = Quaternion.LookRotation(dir);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,qua,rotSpeed*Time.deltaTime);
-        }
-        if(Input.GetButtonDown("Jump")&&groundedPlayer)
-        {
-            PlayerVelocity.y += jumpHeight * -1.0f * gravity;
+            PlayerVelocity.y += jumpHeight * -0.25f * gravity;
         }
 
         PlayerVelocity.y += gravity * Time.deltaTime;
         charaCon.Move(PlayerVelocity * Time.deltaTime);
     }
+
 }
